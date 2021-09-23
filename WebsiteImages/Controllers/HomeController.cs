080405787093b5cf69cd8cd8ebb831ecddbc420b
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2021-09-23 11:12:38 +0700

    Add project files.

---
+++ WebsiteImages/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteImages.Models;
+
+
+namespace WebsiteImages.Controllers
+{
+    public class HomeController : Controller
+    {
+        ImagesEntities db = new ImagesEntities();
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebsiteImages/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebsiteImages.MvcApplication" Language="C#" %>
