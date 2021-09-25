8915426625140c0e018fc855a1d4405eda6aedbe
Apocalysed <phamhuynhanhtien.12a20.2019@gmail.com>
2021-09-25 13:58:36 +0700

    bt đò án ngày 2

--- WebsiteImages/Controllers/HomeController.cs
+++ WebsiteImages/Controllers/HomeController.cs
@@ -13,7 +13,10 @@ namespace WebsiteImages.Controllers
         ImagesEntities db = new ImagesEntities();
         public ActionResult Index()
         {
-
+            List<WebsiteImages.Models.Image> NewImage = db.Images.OrderByDescending(n => n.CreatedDate).Take(8).ToList();
+            ViewBag.NewImage = NewImage;
+            List<WebsiteImages.Models.Image> PopularImage = db.Images.OrderByDescending(p=>p.Views).Take(8).ToList();
+            ViewBag.PopularImage = PopularImage;
             return View();
         }
     }
